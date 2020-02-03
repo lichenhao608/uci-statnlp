17d8be51a7faec67e60eb7c9acaf70e5c48d8927
Chenhao Li <chenhaouci@gmail.com>
2020-02-02 17:40:20 -0800

    trigram model

--- hw2/generator.py
+++ hw2/generator.py
@@ -7,9 +7,10 @@ import random
 from math import log
 import numpy as np
 
+
 class Sampler:
 
-    def __init__(self, lm, temp = 1.0):
+    def __init__(self, lm, temp=1.0):
         """Sampler for a given language model.
 
         Supports the use of temperature, i.e. how peaky we want to treat the
@@ -22,7 +23,7 @@ class Sampler:
         self.rnd = random.Random()
         self.temp = temp
 
-    def sample_sentence(self, prefix = [], max_length = 100):
+    def sample_sentence(self, prefix=[], max_length=100):
         """Sample a random sentence (list of words) from the language model.
 
         Samples words till either EOS symbol is sampled or max_length is reached.
@@ -37,7 +38,7 @@ class Sampler:
             i += 1
         return sent
 
-    def sample_next(self, prev, incl_eos = True):
+    def sample_next(self, prev, incl_eos=True):
         """Samples a single word from context.
 
         Can be useful to debug the model, for example if you have a bigram model,
@@ -47,7 +48,7 @@ class Sampler:
         incl_eos determines whether the space of words should include EOS or not.
         """
         wps = []
-        tot = -np.inf # this is the log (total mass)
+        tot = -np.inf  # this is the log (total mass)
         for w in self.lm.vocab():
             if not incl_eos and w == "END_OF_SENTENCE":
                 continue
@@ -56,23 +57,23 @@ class Sampler:
             tot = np.logaddexp2(lp/self.temp, tot)
         p = self.rnd.random()
         word = self.rnd.choice(wps)[0]
-        s = -np.inf # running mass
-        for w,lp in wps:
+        s = -np.inf  # running mass
+        for w, lp in wps:
             s = np.logaddexp2(s, lp)
             if p < pow(2, s-tot):
                 word = w
                 break
         return word
 
+
 if __name__ == "__main__":
     from lm import Unigram
     unigram = Unigram()
     corpus = [
-        [ "sam", "i", "am" ]
+        ["sam", "i", "am"]
     ]
     unigram.fit_corpus(corpus)
     print(unigram.model)
     sampler = Sampler(unigram)
     for i in range(10):
         print(i, ":", " ".join(str(x) for x in sampler.sample_sentence([])))
-
--- hw2/lm.py
+++ hw2/lm.py
@@ -17,6 +17,7 @@ if sys.version_info.major > 2:
     def unicode(*args, **kwargs):
         return str(*args, **kwargs)
 
+
 class LangModel:
     def fit_corpus(self, corpus):
         """Learn the language model for the whole corpus.
@@ -32,7 +33,7 @@ class LangModel:
         Assumes the model uses an EOS symbol at the end of each sentence.
         """
         vocab_set = set(self.vocab())
-        words_set  = set([w for s in corpus for w in s])
+        words_set = set([w for s in corpus for w in s])
         numOOV = len(words_set - vocab_set)
         return pow(2.0, self.entropy(corpus, numOOV))
 
@@ -40,7 +41,7 @@ class LangModel:
         num_words = 0.0
         sum_logprob = 0.0
         for s in corpus:
-            num_words += len(s) + 1 # for EOS
+            num_words += len(s) + 1  # for EOS
             sum_logprob += self.logprob_sentence(s, numOOV)
         return -(1.0/num_words)*(sum_logprob)
 
@@ -59,6 +60,7 @@ class LangModel:
     def cond_logprob(self, word, previous, numOOV): pass
     # required, the list of words the language model suports (including EOS)
     def vocab(self): pass
+
 
 class Unigram(LangModel):
     def __init__(self, unk_prob=0.0001):
@@ -93,3 +95,98 @@ class Unigram(LangModel):
 
     def vocab(self):
         return self.model.keys()
+
+
+class Trigram(LangModel):
+    def __init__(self, unk_prob=1e-4, gamma=0, smooth=1):
+        self.model = {}
+        self.lam = smooth
+        self.num_gamma = gamma
+        self.filter = Unigram()
+
+        self.vocab_set = set()
+        self.rare = set()
+        self.num_count = {}
+        self.lunk_prob = log(unk_prob, 2)
+        self.num_word = 0
+
+    def inc_word(self, previous, w):
+        if previous not in self.model:
+            self.model[previous] = {}
+        if w not in self.model[previous]:
+            self.model[previous][w] = 0.0
+        self.model[previous][w] += 1.0
+
+    def fit_sentence(self, sentencce):
+        for i, w in enumerate(sentencce):
+            if i == 0:
+                self.inc_word(('START_OF_SENTENCE', 'START_OF_SENTENCE'), w)
+            elif i == 1:
+                self.inc_word(('START_OF_SENTENCE', sentencce[i - 1]), w)
+            else:
+                self.inc_word((sentencce[i - 2], sentencce[i - 1]), w)
+            self.filter.inc_word(w)
+        self.inc_word((sentencce[-2], sentencce[-1]), 'END_OF_SENTENCE')
+        self.filter.inc_word('END_OF_SENTENCE')
+
+    def norm(self):
+        for key in self.filter.model:
+            if self.filter.model[key] >= self.num_gamma:
+                self.vocab_set.add(key)
+            else:
+                self.rare.add(key)
+        self.vocab_set.add('UNK')
+
+        # handling rare conditional words
+        for previous in list(self.model.keys()):
+            if previous[0] in self.rare or previous[1] in self.rare:
+                prev = tuple(
+                    w if w in self.vocab_set else 'UNK' for w in previous)
+                if prev not in self.model:
+                    self.model[prev] = {}
+
+                for w, num in self.model[previous].items():
+                    if w not in self.model[prev]:
+                        self.model[prev][w] = 0.0
+                    self.model[prev][w] += num
+
+                del self.model[previous]
+
+        # make sure that when testing there is situation that is unk,unk
+        if ('UNK', 'UNK') not in self.model:
+            self.model[('UNK', 'UNK')] = {}
+
+        # smoothing
+        for previous in list(self.model.keys()):
+            tot = sum(self.model[previous].values())
+
+            # unknown
+            for w in self.rare:
+                if w in self.model[previous]:
+                    if 'UNK' not in self.model[previous]:
+                        self.model[previous]['UNK'] = 0.0
+                    self.model[previous]['UNK'] += self.model[previous][w]
+                    del self.model[previous][w]
+
+            # smoothing
+            for w in self.vocab_set:
+                if w not in self.model[previous]:
+                    self.model[previous][w] = 0.0
+                self.model[previous][w] += self.lam
+                self.model[previous][w] /= (self.lam *
+                                            len(self.vocab_set) + tot)
+
+    def cond_logprob(self, word, previous, numOOV):
+        if len(previous) < 2:
+            for _ in range(2 - len(previous)):
+                previous = ['START_OF_SENTENCE'] + previous
+
+        prev = tuple(
+            w if w in self.vocab_set else 'UNK' for w in previous[len(previous)-2:])
+        if word in self.model[prev]:
+            return self.model[prev][word]
+        else:
+            return log(self.model[previous]['UNK'], 2) - log(numOOV, 2)
+
+    def vocab(self):
+        return list(self.vocab_set)
